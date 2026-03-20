98e0d1bd91b96e7a43d39e1c0116e1c58a61cba2
yosemm <140467686+yosemm@users.noreply.github.com>
2026-03-19 20:15:24 -0600

    Agregar funcion para construirPerfil y renderizarPerfil

--- app.js
+++ app.js
@@ -10,3 +10,30 @@ function mostrarEtiquetas(habilidades) {
         etiquetas.appendChild(span);
     });
 }
+
+function construirPerfil(datos) {
+    let perfil = {
+        perfilName: datos.name,
+        usuario: datos.login,
+        email: datos.email !== null ? datos.email : "No disponible",
+        ciudad: datos.location !== null ? datos.location : "Sin ubicación",
+        avatar: datos.avatar_url
+    };
+
+    return perfil;
+}
+
+function renderizarPerfil(perfil) {
+    // Elementos del DOM
+    let nombre = document.querySelector("#nombre");
+    let usuario = document.querySelector("#usuario");
+    let email = document.querySelector("#email");
+    let ciudad = document.querySelector("#ciudad");
+    let avatar = document.querySelector("#avatar");
+    // Actualizarlos con valores de perfil
+    nombre.textContent = perfil.perfilName;
+    usuario.textContent = perfil.usuario;
+    email.textContent = perfil.email;
+    ciudad.textContent = perfil.ciudad;
+    avatar.setAttribute("src", perfil.avatar);
+}
